3e55aa31009dbf5bded7c64ebc0795eb04880531
Adilwebpy <adiltazhibaev42@gmail.com>
2020-10-13 22:28:52 +0600

    Finish create-html

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,6 @@ const jsLoaders = () => {
          },
       },
    ];
-   if (isDev) {
-      loaders.push("eslint-loader");
-   }
-   return loaders;
 };
 module.exports = {
    context: path.resolve(__dirname, "src"),
